4ec9c52a9b3f9ce7343e755fa2c4661c0f4181dc
CamiloOrtiz1 <camilo.ortiz@est.fi.uncoma.edu.ar>
2025-08-28 12:51:00 -0300

    Correccion HiloEnvio

--- HiloEnvio.java
+++ HiloEnvio.java
@@ -1,31 +1,28 @@
-import java.util.Random;
 import java.util.concurrent.Callable;
-import java.util.random.*;
 
 public class HiloEnvio implements Callable<Envio> {
     private Envio envio;
-    private Random numAl;
 
     public HiloEnvio(Envio envio) {
         this.envio = envio;
-        numAl = new Random();
     }
 
     @Override
     public Envio call() throws Exception {
         
-        System.out.println("El envio :" + Thread.currentThread().getName() + "Ha sido creado");
-        if(numAl.nextInt(100) < 29){
-            envio.cancelarEnvio();
-        }else{
-            envio.avanzarEnvio();
+        System.out.println("El envio : " + Thread.currentThread().getName() + " Ha sido creado");
+        Thread.sleep(1000);
+
+        // Simular aleatoriamente la cancelacion
+        if (Math.random() < 0.3) {
+            this.envio.cancelarEnvio();
+            System.out.println(Thread.currentThread().getName() + ". Estado actual: " + this.envio.getEstado().getClass().getSimpleName());
+        } else {
+            this.envio.avanzarEnvio();
+            Thread.sleep(1000);
+            System.out.println(Thread.currentThread().getName() + ". Estado actual: " + this.envio.getEstado().getClass().getSimpleName());
+            this.envio.avanzarEnvio();
         }
-        
-        if(numAl.nextInt(100)<14){
-            envio.cancelarEnvio();
-        }else{
-            envio.avanzarEnvio();
-        }
-        return envio; 
+        return envio;
     }
 }
