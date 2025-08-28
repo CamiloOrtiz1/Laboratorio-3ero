0d88a50ab2264d2afc22dc985aeee825c20d352d
CamiloOrtiz1 <camilo.ortiz@est.fi.uncoma.edu.ar>
2025-08-28 12:56:19 -0300

    Cambios locales en Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,21 @@
+import java.util.concurrent.*;
+
 public class Main {
     public static void main(String[] args) {
-        
-        Envio envio = new Envio();
+        ExecutorService executor = Executors.newFixedThreadPool(4);
 
-        //new Menu(envio).display();
+        try {
+            for(int i=0; i < 10; i++) {
+            Envio envio = new Envio();
+            HiloEnvio tarea = new HiloEnvio(envio);
+            Future<Envio> future = executor.submit(tarea);
+            System.out.println(future.get())    ;
+            }
+            
+        } catch(InterruptedException | ExecutionException e) {
+            System.out.println("Error");
+        } finally {
+            executor.shutdown();
+        }
     }
 }
